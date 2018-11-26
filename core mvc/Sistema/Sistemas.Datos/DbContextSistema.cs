a18e5ca241a98f86f94195171f760bfb4e0fc691
omegaxl1 <josepullaguarixl1@gmail.com>
2018-11-25 21:32:05 -0500

    Core Mvc
    practica 1

---
+++ core mvc/Sistema/Sistemas.Datos/DbContextSistema.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sistema.Entidades.Almacen;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static Sistema.Datos.Mapping.Almacen.CategoriaMap;
+
+namespace Sistema.Datos
+{
+   public class DbContextSistema : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbContextSistema(DbContextOptions<DbContextSistema> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration ( new CategariaMap() );
+        }
+    }
+}
