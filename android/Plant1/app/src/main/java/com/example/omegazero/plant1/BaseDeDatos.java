de1554e73fa79473b150660d55047bd2bb68f85b
omegaxl1 <josepullaguarixl1@gmail.com>
2018-12-26 06:42:11 -0500

    cambios2
    cambios

--- android/Plant1/app/src/main/java/com/example/omegazero/plant1/BaseDeDatos.java
+++
@@ -1,9 +0,0 @@
-package com.example.omegazero.plant1;
-
-/**
- * Created by Omega Zero on 25/09/2016.
- */
-public class BaseDeDatos {
-
-    String [] planetas =new String[]{"Venus","Planeta1","Planeta2"};
-}
