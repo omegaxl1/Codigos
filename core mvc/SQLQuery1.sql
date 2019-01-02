dc74eced84b82c1b20d9fc05c19dc8f0f2cbbc21
omegaxl1 <josepullaguarixl1@gmail.com>
2019-01-01 21:25:02 -0500

    Update SQLQuery1.sql

--- core mvc/SQLQuery1.sql
+++ core mvc/SQLQuery1.sql
@@ -11,3 +11,42 @@ INSERT INTO categoria (nombre, descripcion) VALUES ('item1','');
 
 
 "Conexion": "data source=.\\SQLEXPRESS;initial catalog=dbsistema;user id=usuario;password=11231321321;persist security info=True;"
+
+-*****************************-
+
+<html>
+<head>
+    <script language="JavaScript">
+ 
+    /* Determinamos el tiempo total en segundos */
+    var totalTiempo=600;
+ 
+ 
+ 
+    function updateReloj()
+    {
+        document.getElementById('CuentaAtras').innerHTML = "*** "+totalTiempo+" segundos restantes";
+ 
+        if(totalTiempo==0)
+        {
+            alert ('fin')
+        }else{
+            /* Restamos un segundo al tiempo restante */
+            totalTiempo-=1;
+            /* Ejecutamos nuevamente la función al pasar 1000 milisegundos (1 segundo) */
+            setTimeout("updateReloj()",1000);
+        }
+    }
+ 
+    window.onload=updateReloj;
+ 
+    </script>
+</head>
+ 
+<body>
+ 
+<h1>Tiempo restante </h1>
+<h2 id='CuentaAtras'></h2>
+ 
+</body>
+</html>
